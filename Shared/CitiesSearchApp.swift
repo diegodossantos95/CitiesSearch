717ff61054e0cebd76106bddc5bf398df82dc811
Diego Dos Santos <>
2021-04-23 16:37:43 -0300

    First commit

---
+++ Shared/CitiesSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  CitiesSearchApp.swift
+//  Shared
+//
+//  Created by Diego Dos Santos on 22/04/21.
+//
+
+import SwiftUI
+
+@main
+struct CitiesSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
